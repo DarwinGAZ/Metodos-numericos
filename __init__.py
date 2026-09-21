6be987294a3ab64de6be8525966d75df55a59c6c
ns <nelsonspinellineto@gmail.com>
2026-09-21 13:50:35 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-"""Pacote com métodos numéricos para determinação de raízes."""
-from .metodos import bissecao, posicao_falsa, ponto_fixo, secante, newton_raphson
-from .problema import f, df, g
-
-__all__ = ["bissecao", "posicao_falsa", "ponto_fixo", "secante",
-           "newton_raphson", "f", "df", "g"]
